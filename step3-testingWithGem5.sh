12fe0e0846ddb9a8fbb0eba57dd0b367f6defdbc
ali10129 <10778765+ali10129@users.noreply.github.com>
2024-08-05 17:30:59 +0000

    fixing relative path issues with `step3-sh`

--- step3-testingWithGem5.sh
+++ step3-testingWithGem5.sh
@@ -3,7 +3,7 @@
 BOLD_GREEN="\t\e[1;32m==> "
 TEXT_RST=" <==\e[0m"
 
-main_dir="$(pwd)/simulator"
+main_dir="$(pwd)"
 
 ##############################################################################
 
@@ -17,13 +17,13 @@ if [ "$1" != "-s" ]; then		# if `-s` is passed then skip these commands
 	echo -e "$BOLD_GREEN Compiling ./build/X86/gem5.opt, ./util/m5, ./util/m5term $TEXT_RST"
 	echo -e "$BOLD_GREEN Conform if needed by pressing enter or y and enter. $TEXT_RST"
 
-	cd "$main_dir/gem5"
+	cd "$main_dir/simulator/gem5"
 	python3 `which scons` -j$(nproc) CDNCcimFS=1 ./build/X86/gem5.opt
 
-	cd "$main_dir/gem5/util/m5"
+	cd "$main_dir/simulator/gem5/util/m5"
 	python3 `which scons` build/x86/out/m5
 
-	cd "$main_dir/gem5/util/term"
+	cd "$main_dir/simulator/gem5/util/term"
 	gcc -o m5term term.c
 
 	cd "$main_dir"
@@ -31,9 +31,10 @@ fi
 
 echo -e "$BOLD_GREEN Running OurConfig.py $TEXT_RST"
 
-$main_dir/gem5/build/X86/gem5.opt --debug-flags=CIMDBG $main_dir/gem5/configs/CDNCcimFS/OurConfig.py
-
+cd "$main_dir/simulator/gem5"
+./build/X86/gem5.opt --debug-flags=CIMDBG ./configs/CDNCcimFS/OurConfig.py
+cd "$main_dir"
 ### in another vscode terminal instance, run:
-##> 		./gem5/util/term/m5term 127.0.0.1 3456
+##> 		./simulator/gem5/util/term/m5term 127.0.0.1 3456
 
 echo -e "$BOLD_GREEN *** DONE *** $TEXT_RST"
